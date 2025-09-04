c0a57563361a5d3f9de94f7d1af3a38422118c60
João Pedro <joaopedrosilvap2001@gmail.com>
2025-09-04 10:44:21 -0300

    adeus

--- grid.html/function.js
+++
@@ -1,8 +0,0 @@
-for (var pizza = 12; pizza >= 0; pizza -= 2) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  if (pizza > 0) {
-    console.log("Eu tenho " + pizza + " pedaços de pizza sobrando");
-  } else {
-    console.log("Acabou a pizza :(");
-  }
-}
--- grid.html/node_21a2ba51250bd.tmp.js
+++
@@ -1,7 +0,0 @@
-for (let pizza = 12; pizza > 1; pizza -= 2) {
-  console.log("Eu tenho  " + pizza + " pedaços de pizza sobrando");
-}
-
-for (let i = 0; (i) => 10; i++) {
-  console.log(i);
-}
