cb36cba89132411293ccf51a1fc2b3f4729cf561
João Pedro <joaopedrosilvap2001@gmail.com>
2025-09-05 18:12:42 -0300

    isso

--- prototipoA3/A3.js
+++ prototipoA3/A3.js
@@ -1,9 +1,9 @@
-var txt = "!Projeto Baja!";
-var speed = 50;
+var txt = "!PROJETO BAJA!";
+var speed = 100;
 var titulo = document.getElementById("titulo_baja");
 
 for (let i = 0; i < txt.length; i++) {
   setTimeout(function () {
-    titulo.innerHTML += txt.charAt(i);
+    document.getElementById("titulo_baja").innerHTML += txt.charAt(i);
   }, i * speed);
 }
