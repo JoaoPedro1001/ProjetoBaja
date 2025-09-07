30007734670bf15bcc94602f5a46615f992a0c0b
João Pedro <joaopedrosilvap2001@gmail.com>
2025-09-07 10:13:11 -0300

    erro

--- prototipoA3/A3.js
+++
@@ -1,9 +0,0 @@
-var txt = "!PROJETO BAJA!";
-var speed = 100;
-var titulo = document.getElementById("titulo_baja");
-
-for (let i = 0; i < txt.length; i++) {
-  setTimeout(function () {
-    document.getElementById("titulo_baja").innerHTML += txt.charAt(i);
-  }, i * speed);
-}
